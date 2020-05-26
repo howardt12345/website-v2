c2a23752c12390706485f7b67cfcbf83e4642d8c
Howard Tseng <howardt12345@gmail.com>
2020-05-26 13:00:15 -0400

    renamed to portfolio (plz don't break)

--- src/config.js
+++ src/config.js
@@ -47,8 +47,8 @@ module.exports = {
       url: '/about'
     },
     {
-      name: "Photos",
-      url: '/photos'
+      name: "Portfolio",
+      url: '/portfolio'
     },
     {
       name: "Projects",
