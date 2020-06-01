1b6b4b5810a66fc8ccc665120d1e0d51ae197ff9
Howard Tseng <howardt12345@gmail.com>
2020-06-01 16:06:16 -0400

    added some functions

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,23 +1,23 @@
 export const throttle = (func, wait = 100) => {
-    let timer = null;
-    return function(...args) {
-      if (timer === null) {
-        timer = setTimeout(() => {
-          func.apply(this, args);
-          timer = null;
-        }, wait);
-      }
-    };
+  let timer = null;
+  return function(...args) {
+    if (timer === null) {
+      timer = setTimeout(() => {
+        func.apply(this, args);
+        timer = null;
+      }, wait);
+    }
   };
+};
   
 export const hex2rgba = (hex, alpha = 1) => {
   const [r, g, b] = hex.match(/\w\w/g).map(x => parseInt(x, 16));
   return `rgba(${r},${g},${b},${alpha})`;
 };
 
-export const replaceAll = (string, search, replace) => {
-  return string.split(search).join(replace);
-}
+export const replaceAll = (string, search, replace) => string.split(search).join(replace);
+
+export const capitalize = (string) => string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 
 export const currentTime = () => {
   let now = new Date();
