3787b5290904f356554b7559b0b2fe312002dac0
Howard Tseng <howardt12345@users.noreply.github.com>
2020-12-27 16:31:45 -0500

    Pruned packages

--- src/pages/about.js
+++ src/pages/about.js
@@ -34,7 +34,7 @@ export const pageQuery = graphql`
           avatar {
             childImageSharp {
               fluid(maxWidth: 700, quality: 90, traceSVG: { color: "#64ffda" }) {
-                ...GatsbyImageSharpFluid_withWebp_tracedSVG
+                ...GatsbyImageSharpFluid
               }
             }
           }
