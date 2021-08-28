d0f12f791bff11d1c555779ab64f66cb94de7144
Howard Tseng <howardt12345@users.noreply.github.com>
2021-08-27 22:44:13 -0400

    Applied Prettier Changes

--- src/pages/photography.js
+++ src/pages/photography.js
@@ -1,77 +1,82 @@
-import React, { useEffect, useState }  from 'react';
-import firebase from "gatsby-plugin-firebase";
-import styled from 'styled-components';
-import { Helmet } from 'react-helmet';
-import { isEmpty, replaceAll } from "@utils";
-import { Layout } from '@components';
-import { LoadingPage, TilesPage } from '@components/photography';
+import React, { useEffect, useState } from "react"
+import firebase from "gatsby-plugin-firebase"
+import styled from "styled-components"
+import { Helmet } from "react-helmet"
+import { isEmpty, replaceAll } from "@utils"
+import { Layout } from "@components"
+import { LoadingPage, TilesPage } from "@components/photography"
 
-const url = "https://firebasestorage.googleapis.com/v0/b/portfolio-49b69.appspot.com/o/";
-const token = "ea925040-1fca-4eda-b1e8-0eb96567ab7e";
+const url =
+  "https://firebasestorage.googleapis.com/v0/b/portfolio-49b69.appspot.com/o/"
+const token = "ea925040-1fca-4eda-b1e8-0eb96567ab7e"
 
 const StyledSection = styled.section`
   margin: auto 0;
   padding: 100px 0 0px;
-`;
+`
 
 class Picture {
   constructor(key, value) {
-    this.date = key;
-    this.name = value.split('"')[1];
-    this.width = parseInt(value.split(",")[1]);
-    this.height = parseInt(value.split(",")[2]);
+    this.date = key
+    this.name = value.split('"')[1]
+    this.width = parseInt(value.split(",")[1])
+    this.height = parseInt(value.split(",")[2])
   }
 
-  getUrl = () => `${url}photo%2F${replaceAll(this.name, ' ', '%20')}?alt=media&token=${token}`;
+  getUrl = () =>
+    `${url}photo%2F${replaceAll(
+      this.name,
+      " ",
+      "%20"
+    )}?alt=media&token=${token}`
 }
 
 const fromFirestore = async () => {
-  let list = [];
+  let list = []
 
-  await firebase.firestore()
-  .collection("photo")
-  .doc("all")
-  .get().then(response => {
-      const data = response.data();
-      const map = data.photos;
-      for(const [key, value] of Object.entries(map)) {
-        list.push(new Picture(key, value));
+  await firebase
+    .firestore()
+    .collection("photo")
+    .doc("all")
+    .get()
+    .then(response => {
+      const data = response.data()
+      const map = data.photos
+      for (const [key, value] of Object.entries(map)) {
+        list.push(new Picture(key, value))
       }
-  });
+    })
 
-  return list;
+  return list
 }
 
 const PhotographyPage = () => {
-
-  const [isLoading, setIsLoading] = useState(true);
-  const [fetching, setFetching] = useState(false);
-  const [data, setData] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(true)
+  const [fetching, setFetching] = useState(false)
+  const [data, setData] = useState([])
 
   useEffect(() => {
-
     async function fetchData() {
       try {
         await firebase.auth().signInAnonymously()
-        let tmp = await fromFirestore();
-        setData(tmp);
-        setIsLoading(false);
-        await firebase.auth().currentUser.delete().then(() => {
-        });
-        setFetching(false);
-      } catch(e) {
-        setIsLoading(false);
+        let tmp = await fromFirestore()
+        setData(tmp)
+        setIsLoading(false)
+        await firebase
+          .auth()
+          .currentUser.delete()
+          .then(() => {})
+        setFetching(false)
+      } catch (e) {
+        setIsLoading(false)
       }
     }
 
-    if(isEmpty(data) && isLoading && !fetching) {
-      setFetching(true);
-      fetchData();
+    if (isEmpty(data) && isLoading && !fetching) {
+      setFetching(true)
+      fetchData()
     }
-
-  }, [isLoading, fetching, data]);
-
+  }, [isLoading, fetching, data])
 
   return (
     <Layout animateNav={false} isHome={false} footer={true}>
@@ -81,15 +86,11 @@ const PhotographyPage = () => {
       </Helmet>
 
       <StyledSection>
-        {!isLoading && (
-          <TilesPage data={data ?? []} />
-        )}
-        {isLoading && (
-          <LoadingPage />
-        )}
+        {!isLoading && <TilesPage data={data ?? []} />}
+        {isLoading && <LoadingPage />}
       </StyledSection>
     </Layout>
-  );
+  )
 }
 
-export default PhotographyPage;
+export default PhotographyPage
